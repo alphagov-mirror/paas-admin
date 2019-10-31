94f4a7eaa3e12b3d2f511d1b7833ecb91b668333
Mohamed Deerow <mohamed.deerow@digital.cabinet-office.gov.uk>
2019-10-31 15:10:55 +0000

    WIP Add service metrics tab
    Currently missing:
    
    * tests
    * any actual metrics

---
+++ src/components/service-metrics/service-metrics.ts
@@ -0,0 +1,61 @@
+import { IContext } from '../app';
+import CloudFoundryClient from '../../lib/cf';
+import { CLOUD_CONTROLLER_ADMIN, CLOUD_CONTROLLER_GLOBAL_AUDITOR, CLOUD_CONTROLLER_READ_ONLY_ADMIN } from '../auth';
+import { IParameters, IResponse } from '../../lib/router';
+import { fromOrg, IBreadcrumb } from '../breadcrumbs';
+import serviceMetricsTemplate from './service-metrics.njk';
+
+
+export async function viewServiceMetrics(ctx: IContext, params: IParameters): Promise<IResponse> {
+    const cf = new CloudFoundryClient({
+        accessToken: ctx.token.accessToken,
+        apiEndpoint: ctx.app.cloudFoundryAPI,
+        logger: ctx.app.logger,
+      });
+
+    const isAdmin = ctx.token.hasAnyScope(
+        CLOUD_CONTROLLER_ADMIN,
+        CLOUD_CONTROLLER_READ_ONLY_ADMIN,
+        CLOUD_CONTROLLER_GLOBAL_AUDITOR,
+    );
+
+    const [isManager, isBillingManager, userProvidedServices, space, organization] = await Promise.all([
+        cf.hasOrganizationRole(params.organizationGUID, ctx.token.userID, 'org_manager'),
+        cf.hasOrganizationRole(params.organizationGUID, ctx.token.userID, 'billing_manager'),
+        cf.userServices(params.spaceGUID),
+        cf.space(params.spaceGUID),
+        cf.organization(params.organizationGUID),
+    ]);
+
+    const isUserProvidedService = userProvidedServices.some(s => s.metadata.guid === params.serviceGUID);
+
+    const service = isUserProvidedService ?
+      await cf.userServiceInstance(params.serviceGUID) :
+      await cf.serviceInstance(params.serviceGUID);
+
+    const breadcrumbs: ReadonlyArray<IBreadcrumb> = fromOrg(ctx, organization, [
+        {
+            text: space.entity.name,
+            href: ctx.linkTo('admin.organizations.spaces.services.list', {
+            organizationGUID: organization.metadata.guid,
+            spaceGUID: space.metadata.guid,
+            }),
+        },
+        { text: service.entity.name },
+    ]);
+
+    return {
+        body: serviceMetricsTemplate.render({
+            routePartOf: ctx.routePartOf,
+            linkTo: ctx.linkTo,
+            context: ctx.viewContext,
+            organization,
+            service,
+            space,
+            isAdmin,
+            isBillingManager,
+            isManager,
+            breadcrumbs,
+        }),
+    };
+}
